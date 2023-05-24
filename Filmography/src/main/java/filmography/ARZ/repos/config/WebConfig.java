6a7374cd61220f163e16372d700973cc11c392ca
Killer Albertha <killeral@va-g3.kzn.21-school.ru>
2023-05-24 13:02:00 +0300

    DAO&SERVICE

--- Filmography/src/main/java/filmography/ARZ/repos/config/WebConfig.java
+++ Filmography/src/main/java/filmography/ARZ/repos/config/WebConfig.java
@@ -1,22 +1,53 @@
 package filmography.ARZ.repos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan("filmography.ARZ.repos")
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public WebConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/Pages/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/Pages/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
     }
 }
--- Filmography/src/main/java/filmography/ARZ/repos/controller/FilmController.java
+++ Filmography/src/main/java/filmography/ARZ/repos/controller/FilmController.java
@@ -1,21 +1,31 @@
 package filmography.ARZ.repos.controller;
 
+import filmography.ARZ.repos.Model.Film;
+import filmography.ARZ.repos.service.FilmService;
+import filmography.ARZ.repos.service.FilmServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class FilmController {
+    private final FilmService service = new FilmServiceImpl();
     @GetMapping("/")
-    public ModelAndView allFilms() {
+    public String allFilms(Model model) {
+        List<Film> filmList = service.allFilms();
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("film");
-        return modelAndView;
+        modelAndView.addObject("filmList", filmList);
+        return "film";
     }
     @GetMapping("/edit")
-    public ModelAndView editPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("editPage");
-        return modelAndView;
+    public String editPage(Model model) {
+        return "editPage";
+    }
+    @GetMapping("/add")
+    public String addPage(Model model) {
+        return "add";
     }
 }
